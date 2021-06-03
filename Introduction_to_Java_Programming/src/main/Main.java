c583516f1d35b25b216859e7159a02c655fac818
emaphis <emaphis85@gmail.com>
2021-06-03 17:15:24 -0400

    Ok.

--- Introduction_to_Java_Programming/src/main/Main.java
+++ Introduction_to_Java_Programming/src/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
         var hello = "Hello, Intro";
-        System.out.println("Hello, Intro");
+        System.out.println(hello);
     }
 
 }
