b54fb22fe35c7b8d6da4d74b6b5bd637b5f7b8e0
emaphis <emaphis85@gmail.com>
2021-07-26 23:07:24 -0400

    ok

---
+++ Introduction_to_Java_Programming/src/chapter03/exercise/exercise03_21/DayOfWeek.java
@@ -0,0 +1,32 @@
+/*
+ * Exercise 3.21
+ *  Science: day of the week) Zeller’s congruence is an algorithm developed by
+ *  Christian Zeller to calculate the day of the week. The formula is
+ *
+ *    h =
+ *
+ *  ■ h is the day of the week (0: Saturday, 1: Sunday, 2: Monday, 3: Tuesday, 4:
+ *    Wednesday, 5: Thursday, and 6: Friday).
+ *  ■ q is the day of the month.
+ *  ■ m is the month (3: March, 4: April, ..., 12: December). January and February
+ *    are counted as months 13 and 14 of the previous year
+ *  ■ j is year / 100
+ *  ■   k is the year of the century (i.e., year % 100).
+ *
+ *  Note all divisions in this exercise perform an integer division. Write a program
+ *  that prompts the user to enter a year, month, and day of the month, and displays
+ *  the name of the day of the week.
+ */
+package chapter03.exercise.exercise03_21;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emaph
+ */
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
