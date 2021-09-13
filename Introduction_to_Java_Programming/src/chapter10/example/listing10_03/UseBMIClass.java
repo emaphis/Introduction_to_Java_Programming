a88f0e5c463adc5cdbc7dbeb9135fdb36e55ab42
emaphis <emaphis85@gmail.com>
2021-09-13 17:49:56 -0400

    Check Point 10.3

---
+++ Introduction_to_Java_Programming/src/chapter10/example/listing10_03/UseBMIClass.java
@@ -0,0 +1,21 @@
+/*
+ * Listing 10.3
+ * 10.3 Thinking in Objects
+ */
+package chapter10.example.listing10_03;
+
+/**
+ * Exercise BMI class
+ * @author emaph
+ */
+public class UseBMIClass {
+    public static void main(String[] args) {
+        BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
+        System.out.println("The BMI for " + bmi1.getName() + " is "
+            + bmi1.getBMI() + " " + bmi1.getStatus());
+
+        BMI bmi2 = new BMI("Susan King", 215, 70);
+        System.out.println("The BMI for " + bmi2.getName() + " is "
+            + bmi2.getBMI() + " " + bmi2.getStatus());
+    }
+}
