63da2b9376f7eaeb3c31bb7f878b06fccf3b0d6a
emaphis <emaphis85@gmail.com>
2022-08-10 22:19:42 -0400

    One more passes test

--- Introduction_to_Java_Programming/src/main/java/chapter09/exercise/exercise09_08/Fan.java
+++ Introduction_to_Java_Programming/src/main/java/chapter09/exercise/exercise09_08/Fan.java
@@ -9,6 +9,7 @@ package chapter09.exercise.exercise09_08;
  * @author emaph
  */
 public class Fan {
+    /** Constants that represent Fan speeds */
    public static final int SLOW = 1;
    public static final int MEDIUM = 2;
    public static final int FAST = 3;
@@ -18,6 +19,7 @@ public class Fan {
    private double radius;
    private String color;
 
+   /** Create default Fan */
    public Fan() {
        speed = SLOW;
        on = false;
@@ -25,52 +27,58 @@ public class Fan {
        color = "blue";
    }
 
+   /** set Fan speed */
    public void setSpeed(int newSpeed) {
        speed = newSpeed;
    }
 
-   public String getSpeed() {
-       switch (speed) {
-           case SLOW:
-               return "Slow";
-           case MEDIUM:
-               return "Medium";
-           case FAST:
-               return "Fast";
-           default:
-               return "Unkown";
-       }
+   /** get Fan speed */
+   public int getSpeed() {
+        return speed;
    }
 
+   /** return Fan turned on status */
    public boolean isOn() {
        return on;
    }
 
+   /** turn Fan on or off  */
    public void setOn(boolean isOn) {
        on = isOn;
    }
 
+   /** set Fan radius */
    public void setRadius(double newRadius) {
        radius = newRadius;
    }
 
+   /** get Fan radius */
    public double getRadius() {
        return radius;
    }
 
+   /** set fan color */
    public void setColor(String newColor) {
        color = newColor;
    }
 
+   /** get Fan color */
     public String getColor() {
         return color;
     }
 
+
     @Override
     public String toString() {
+        String out =
+                "speed " + speed +
+                "\ncolor " + color +
+                "\nradius " + radius;
         if (isOn())
-            return "speed= " + getSpeed() + ", color= " + getColor() + ", radius= " + getRadius();
+            out += "\nfan is on\n";
         else
-            return "color= " + getColor() + ", radius= " + getRadius()  + ", fan is off";
+            out += "\nfan is off\n";
+        return out;
     }
+
 }
