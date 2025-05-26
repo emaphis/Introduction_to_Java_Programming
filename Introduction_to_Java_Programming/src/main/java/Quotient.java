7a4f8db2d0017e507f4c72a1c3eac093100b88f0
emaphis <emaphis85@gmail.com>
2025-05-26 14:56:49 -0400

    Add Listing 12.9

---
+++ Introduction_to_Java_Programming/src/main/java/Quotient.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * Listing 12.1
+ * 12.8 Exception-Handling Overview
+ *  Demonstrate exception handling, including how an exception object is
+ *  created and thrown.
+ */
+
+/**
+ * An exception example. 
+ * @author emaph
+ */
+public class Quotient {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Prompt user to enter two integers
+        System.out.print("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        
+        System.out.println(number1 + " / " + number2 + " is " +
+                (number1 / number2));
+    }
+}
+
+/*
+Enter two integers: 5 2
+5 / 2 is 2
+
+Enter two integers: 3 0
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+*/
